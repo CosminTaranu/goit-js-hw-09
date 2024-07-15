509dc685bc187725420825f2bc114c1f14266a70
CosminTaranu <ctaranu83@gmail.com>
2024-07-15 17:13:30 +0300

    comit-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
 
+  const startBtn = document.querySelector('button[data-start]');
+  const stopBtn= document.querySelector('button[data-stop]');
+
+  let colorChangeInterval ;
+
+  startBtn.addEventListener ('click', () => {
+    startBtn.disabled = true;
+
+    colorChangeInterval = setInterval (() => {
+        document.body.style.backgroundColor = getRandomHexColor ();
+    }, 1000);
+  });
+
+  stopBtn.addEventListener ('click', () =>{
+    clearInterval(colorChangeInterval);
+    startBtn.disabled = false;
+  });
